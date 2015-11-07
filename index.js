02bf4026e846cc8db168ccbb424364c27dc33605
spolischook <spolischook@gmail.com>
2015-11-07 16:19:14 +0200

    Make wss connection instead of ws

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+var fs    = require("fs"),
+    config = require("config");
+
+var processRequest = function(req, res) {
+    console.log("Request received.")
+};
+
+var httpServ = require('https');
+var app = httpServ.createServer({
+    key: fs.readFileSync(config.ws.ssl_key),
+    cert: fs.readFileSync(config.ws.ssl_cert)
+}, processRequest).listen(config.ws.port);
+
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+  , wss = new WebSocketServer({server: app});
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
